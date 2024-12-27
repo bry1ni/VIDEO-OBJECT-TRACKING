d5395a3d217fea03da78665ad5ca1c0448b0172c
Ryan <r.benatallah01@gmail.com>
2024-12-27 17:17:59 +0100

    restructure

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(r'the/path/to/the/video')
+cap = cv2.VideoCapture(r'videos/video1.mp4')
 
 """SHITOMASI CORNER DETECTION"""
 feature_params = dict(maxCorners=100,
